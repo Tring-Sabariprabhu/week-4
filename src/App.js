ca3e8f6f3515b9fbefc993ed5eb3e21025a1aca5
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-02-14 14:21:20 +0530

    Added fields

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Form  from './Form';
+// import MyComponent from './MyComponent';
+function App() {
+  
+  return (
+ 
+    <div className="App">
+      
+        <Form/>
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/InputField.js
@@ -0,0 +1,12 @@
+export const InputFields=()=>{
+    return(
+        <>
+       
+            <input type="text" placeHolder="Name" name="name" />
+            <input type="number" placeHolder="Age" name="age" />
+            <input type="text" placeHolder="Location" name="location"/>
+        
+        </>
+    );
+};
+
