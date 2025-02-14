72121893f39fa3eabbf3cee25ac1935c067b836b
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-02-14 14:55:45 +0530

    Update FetchData.js

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,7 +1,6 @@
 import {useEffect , useState} from 'react';
 import axios from 'axios';
 import { Card } from './Card';
-import { CardList } from './CardList';
 export const FetchData=()=>{
     const [data, setData] = useState(null);
     const [ loading , setLoading] = useState(true);
