a88d89c9e353780ef2e673c0b90ada10b16198a5
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-02-14 14:39:06 +0530

    Added files

---
+++ src/FetchData.js
@@ -0,0 +1,32 @@
+import {useEffect , useState} from 'react';
+import axios from 'axios';
+import { Card } from './Card';
+import { CardList } from './CardList';
+export const FetchData=()=>{
+    const [data, setData] = useState(null);
+    const [ loading , setLoading] = useState(true);
+
+    useEffect(()=>{
+        axios.get("https://api.restful-api.dev/objects")
+        .then((response) => {
+            setData(response.data);
+            // console.log(response.data);
+            setLoading(false);
+        })
+    },[]);
+    if(loading){
+        return <p>Loading..</p>;
+    }
+    if(data == null){
+        return <p>Error! </p>;
+    }
+    return(
+        <>
+        <div className='CardList'>
+        {data.map((item) => {
+            return (<Card card={item}/>)
+        })}
+        </div>
+        </>
+    );
+};
