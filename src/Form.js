34bbbf5283f9e379d002107d24d55abd2cb4a00d
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-03-23 18:39:44 +0530

    edited css

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,13 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './Form.css';
-import { PopUp } from './PopUp';
 import { useForm } from 'react-hook-form';
 import { makeToast } from './MakeToast';
-export const Form=()=>{
-    const [ inputBoxes, setInputBoxes] = useState([]);
-    const [ Index, setIndex] = useState(null);
-    const [ addButtonState, setAddButtonState] = useState(false);
-    const [ delButtonState, setDelButtonState] = useState(false);
-    const [ editButtonState, setEditButtonState] = useState(false);
+export const Form = () => {
+    const [inputBoxes, setInputBoxes] = useState([]);
+    const [Index, setIndex] = useState(null);
+    const [addButtonState, setAddButtonState] = useState(false);
+    const [delButtonState, setDelButtonState] = useState(false);
+    const [editButtonState, setEditButtonState] = useState(false);
     const {
         register,
         handleSubmit,
@@ -16,200 +15,180 @@ export const Form=()=>{
         setValue,
         reset,
         formState: { errors },
-      } = useForm({defaultValues: {name: "" , age: "", skill: "",designation: "", address : ""}});
+    } = useForm({ defaultValues: { name: "", age: "", skill: "", designation: "", address: "" } });
 
-    const SetAddBtnState=(value)=>{
-        
+    const SetAddBtnState = (value) => {
+
         setAddButtonState(value);
-        if(value){
-            reset({name: "" , age: "", designation: "", skill: "", address: ""});
-            console.log(Index);        
+        if (value) {
+            reset({ name: "", age: "", designation: "", skill: "", address: "" });
         }
-        // setExistData({name: "" , age: "", skill: "",designation: "", address : ""});
+
     }
-    const SetEditBtnState=(value, EditIndex)=>{
+    const SetEditBtnState = (value, EditIndex) => {
         setEditButtonState(value);
-        if(value){
+        if (value) {
             setIndex(EditIndex);
             const ExistData = inputBoxes[EditIndex];
             Object.keys(ExistData).forEach((key) => {
                 setValue(key, ExistData[key]); // Update form fields
-              });
+            });
         }
-        else{
+        else {
             setIndex(null);
         }
     }
-    const SetDelBtnState=(value, DeleteIndex)=>{
+    const SetDelBtnState = (value, DeleteIndex) => {
         setDelButtonState(value);
-        if(value){
+        if (value) {
             setIndex(DeleteIndex);
         }
-        else{
+        else {
             setIndex(null);
         }
     }
-    
-   
+
+
     const onSubmit = (data) => {
-        if(addButtonState){
-            setInputBoxes([...inputBoxes, data]) ;
-            makeToast('Record added','success');
+        if (addButtonState) {
+            setInputBoxes([...inputBoxes, data]);
+            makeToast('Record added', 'success');
             setAddButtonState(false);
         }
-        else if(editButtonState){
+        else if (editButtonState) {
             inputBoxes[Index] = data;
-            makeToast('Record updated','success');
+            makeToast('Record updated', 'success');
             setEditButtonState(false);
         }
     }
-    const ShowErrors=()=>{
-        if(errors.name)
-            makeToast(errors.name.message, 'error');
-       if(errors.age)
-           makeToast(errors.age.message, 'error');
-       if(errors.skill)
-           makeToast(errors.skill.message, 'error');
-       if(errors.designation)
-           makeToast(errors.designation.message, 'error');
-       if(errors.address)
-           makeToast(errors.address.message, 'error');
-    }
-  
-    const AfterDeleteClick=()=>{
+ 
+    const AfterDeleteClick = () => {
         const OldInputBoxes = [...inputBoxes];
         OldInputBoxes.splice(Index, 1);
         setInputBoxes(OldInputBoxes);
-        makeToast('Record deleted','success');
+        makeToast('Record deleted', 'success');
         SetDelBtnState(false, null);
     };
- 
-    const OperationStart=()=>{
-        
-        return(
-            
+
+    const OperationStart = () => {
+
+        return (
             <form className='PopupMessage' onSubmit={handleSubmit(onSubmit)}>
-                { (editButtonState || addButtonState) && <>
-                <div className='flex_row '>
-                    <div className='flex_column Label_div'>
-                        <label htmlFor='name'>Name </label>
-                    
-                        <label htmlFor='age'>Age </label>
-                    
-                        <label htmlFor='skill'>Skill </label>
-                    
-                        <label htmlFor='designation' >Designation </label>
-                    
-                        <label htmlFor='address' >Address </label>
-                    </div>
-                    <div className='flex_column Input_div'>
+                {(editButtonState || addButtonState) && 
+                <>
+                    <div className='Input-div'>
+                        <label>Name</label>
                         <input type="text" name="name" id='name' value={watch("name") || ""}
-                                    {...register("name", {                             
-                                        pattern: {
-                                          value: /^[A-Za-z\s]*$/,
-                                          message: "Name should only contain alphabets",
-                                        },
-                                        validate: {
-                                          requiredCheck: (value) =>   value?.length > 0 || "Name is required",
-                                          SpacesContained: (value) => ( value?.trim()?.length > 0) || "Name Should be valid"
-                                        }
-                                      })} 
-                                    /><br/>
-                        
-                        <input type="text" name="age" id="age" value={watch("age") || ""}
-                                    {...register("age", {                             
-                                        pattern: {
-                                          value: /^[0-9]*$/,
-                                          message: "Age should be only numbers",
-                                        },
-                                        validate: {
-                                          requiredCheck: (value) =>   value?.length > 0 || "Age is required",
-                                          SpacesContained: (value) => ( value?.trim()?.length > 0) || "Age Should be valid"
-                                        }
-                                      })} 
-                                    /><br/>
-                        {/* { errors.name && console.log(errors.name)} */}
-                        <input type="text" name="skill" id="skill" value={watch("skill") || ""}
-                                    {...register("skill", {                             
-                                        pattern: {
-                                          value: /^[A-Za-z\s]*$/,
-                                          message: "Skill should only contain alphabets",
-                                        },
-                                        validate: {
-                                          requiredCheck: (value) =>   value?.length > 0 || "Skill is required",
-                                          SpacesContained: (value) => ( value?.trim()?.length > 0) || "Skill Should be valid"
-                                        }
-                                      })} 
-                                    /><br/>
-                        {/* { errors.name && console.log(errors.name)} */}
-                        <input type="text" name="designation" id="designation" value={watch("designation") || ""}
-                                    {...register("designation", {                             
-                                        pattern: {
-                                            value: /^[A-Za-z\s]*$/,
-                                            message: "Designation should only contain alphabets",
-                                          },
-                                        validate: {
-                                          requiredCheck: (value) =>   value?.length > 0 || "Designation is required",
-                                          SpacesContained: (value) => ( value?.trim()?.length > 0) || "Designation Should be valid"
-                                        }
-                                      })} 
-                                    /><br/>
-                        {/* { errors.name && console.log(errors.name)} */}
-                        <input type="text" name="address" id="address" value={watch("address") || ""}
-                                    {...register("address", {                             
-                                        validate: {
-                                          requiredCheck: (value) =>   value?.length > 0 || "Address is required",
-                                          SpacesContained: (value) => ( value?.trim()?.length > 0) || "Address Should be valid"
-                                        }
-                                      })} 
-                                    /><br/>
-                        {/* { errors.name && console.log(errors.name)} */}
+                            {...register("name", {
+                                pattern: {
+                                    value: /^[A-Za-z\s]*$/,
+                                    message: "Name should only contain alphabets",
+                                },
+                                validate: {
+                                    requiredCheck: (value) => value?.length > 0 || "Name is required",
+                                    SpacesContained: (value) => (value?.trim()?.length > 0) || "Name Should be valid"
+                                }
+                            })} />
                     </div>
-                
-
-                </div>
+                    <p className='error'>{errors?.name?.message}</p>
+                    <div className='Input-div'>
+                        <label>Age</label>
+                        <input type="text" name="age" id="age" value={watch("age") || ""}
+                            {...register("age", {
+                                pattern: {
+                                    value: /^[0-9]*$/,
+                                    message: "Age should be only numbers",
+                                },
+                                validate: {
+                                    requiredCheck: (value) => value?.length > 0 || "Age is required",
+                                    SpacesContained: (value) => (value?.trim()?.length > 0) || "Age Should be valid"
+                                }
+                            })} />
+                    </div>
+                    <p className='error'>{errors?.age?.message}</p>
+                    <div className='Input-div'>
+                        <label>Skill</label>
+                        <input type="text" name="skill" id="skill" value={watch("skill") || ""}
+                            {...register("skill", {
+                                pattern: {
+                                    value: /^[A-Za-z\s]*$/,
+                                    message: "Skill should only contain alphabets",
+                                },
+                                validate: {
+                                    requiredCheck: (value) => value?.length > 0 || "Skill is required",
+                                    SpacesContained: (value) => (value?.trim()?.length > 0) || "Skill Should be valid"
+                                }
+                            })} />
+                    </div>
+                    <p className='error'>{errors?.skill?.message}</p>
+                    <div className='Input-div'>
+                        <label>Designation</label>
+                        <input type="text" name="designation" id="designation" value={watch("designation") || ""}
+                            {...register("designation", {
+                                pattern: {
+                                    value: /^[A-Za-z\s]*$/,
+                                    message: "Designation should only contain alphabets",
+                                },
+                                validate: {
+                                    requiredCheck: (value) => value?.length > 0 || "Designation is required",
+                                    SpacesContained: (value) => (value?.trim()?.length > 0) || "Designation Should be valid"
+                                }
+                            })} />
+                    </div>
+                    <p className='error'>{errors?.designation?.message}</p>
+                    <div className='Input-div'>
+                        <label>Address</label>
+                        <input type="text" name="address" id="address" value={watch("address") || ""}
+                            {...register("address", {
+                                validate: {
+                                    requiredCheck: (value) => value?.length > 0 || "Address is required",
+                                    SpacesContained: (value) => (value?.trim()?.length > 0) || "Address Should be valid"
+                                }
+                            })} />
+                    </div>
+                    <p className='error'>{errors?.address?.message}</p>
                 </>}
-
-                
-                { addButtonState && <>
-                    <button type='submit'
-                                    onClick={ShowErrors} 
-                                    className='yellowButton button1'>Save</button>
-                    <button type='button' 
-                                    onClick={()=>SetAddBtnState(false)}
-                                    className='button2 redButton'>Close</button>
-                    </>}   
-                    { editButtonState && <>
-                        <button type='submit' 
-                                    onClick={ShowErrors} 
-                                    className='yellowButton button1'>Update</button>
-                        <button type='button' 
-                                    onClick={()=>SetEditBtnState(false, null)} 
-                                    className='button2 redButton'>Close</button>
+                <div className='Buttons'>
+                    {addButtonState &&
+                        <>
+                            <button type='submit'
+                                className='yellowButton button1'>Save</button>
+                            <button type='button'
+                                onClick={() => SetAddBtnState(false)}
+                                className='button2 '>Close</button>
                         </>}
-                        {delButtonState && <>
-                                <label>Delete record</label><br/><br/>
-                                <button type='button' 
-                                                onClick={()=>AfterDeleteClick()} 
-                                                className='yellowButton button1'>Confirm</button>
-                                <button type='button' 
-                                                onClick={()=>SetDelBtnState(false, null)} 
-                                                className='button2 redButton'>Cancel</button>
-                                </>}
-              
+                    {editButtonState &&
+                        <>
+                            <button type='submit'
+                                className='yellowButton button1'>Update</button>
+                            <button type='button'
+                                onClick={() => SetEditBtnState(false, null)}
+                                className='button2'>Close</button>
+                        </>}
+                    {delButtonState &&
+                        <>
+                            <label>Delete record</label><br /><br />
+                            <button type='button'
+                                onClick={() => AfterDeleteClick()}
+                                className='redButton button1'>Delete</button>
+                            <button type='button'
+                                onClick={() => SetDelBtnState(false, null)}
+                                className='button2'>Cancel</button>
+                        </>}
+                </div>
             </form>
         )
     }
-   
-    return(
+
+    return (
         <div className='box-container'>
-            
-            <button className='addNewButton' onClick={()=>SetAddBtnState(true)} > Add New</button>
-            
+
+            <button className='addNewButton' onClick={() => SetAddBtnState(true)} > Add New</button>
+
             {
-                addButtonState == true && OperationStart() 
+                addButtonState === true && OperationStart()
             }
-            
+
             <table>
                 <tr>
                     <th>Name</th>
@@ -219,25 +198,25 @@ export const Form=()=>{
                     <th>Address</th>
                     <th>Actions</th>
                 </tr>
-                
-                {inputBoxes.map((value, key)=>(
+
+                {inputBoxes.map((value, key) => (
                     <tr key={key}>
-                    <td>{value.name}</td>
-                    <td>{value.age}</td>
-                    <td>{value.skill}</td>
-                    <td>{value.designation}</td>
-                    <td>{value.address}</td>
-                    <td><button className='bluebutton' onClick={()=>SetEditBtnState(true, key)} > Edit</button>
-                    { editButtonState == true && OperationStart()}
-                    <button className='redButton' onClick={()=>SetDelBtnState(true, key)}> Delete</button>
-                    {
-                        delButtonState == true && OperationStart()
-                        }
-                    </td>
-                </tr>
+                        <td>{value.name}</td>
+                        <td>{value.age}</td>
+                        <td>{value.skill}</td>
+                        <td>{value.designation}</td>
+                        <td>{value.address}</td>
+                        <td><button className='bluebutton' onClick={() => SetEditBtnState(true, key)} > Edit</button>
+                            {editButtonState === true && OperationStart()}
+                            <button className='redButton' onClick={() => SetDelBtnState(true, key)}> Delete</button>
+                            {
+                                delButtonState === true && OperationStart()
+                            }
+                        </td>
+                    </tr>
                 ))}
-                </table>
-                
+            </table>
+
         </div>
     );
 }
--- src/MakeToast.js
+++ src/MakeToast.js
@@ -1,11 +1,10 @@
 import { toast } from "react-toastify";
 export const makeToast = (message, Toasttype) => {
-    // console.log(message);
   toast(message,{
     autoClose: 3000,       
     type: Toasttype,         
     closeOnClick: true,    
-    // pauseOnHover: true,    
+    pauseOnHover: true,    
   });
 };
 
