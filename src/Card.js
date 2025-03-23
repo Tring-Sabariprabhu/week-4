7b6393a633eedfe49a696552798aee8a14a8c20e
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-03-23 18:33:02 +0530

    edited css

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,16 @@
-import './Card.css';
 import phoneImage from './Images/VIVO_Y18.jpg';
-export const Card=(props)=>{
+export const Card = (props) => {
     const obj = props.card.data;
-    return(
+    return (
         <div className="Card">
             <div className='Title'>
                 <h3>{props.card.name}</h3>
             </div>
-            <img src={phoneImage} alt=''/>
+            <img src={phoneImage} alt='' />
             <div className='Content'>
-            { obj!=null && Object.entries(obj).map(([key, value])=>(
-                <h4><span>{key} :</span>{value}</h4>
-            ))}
+                {obj != null && Object.entries(obj).map(([key, value]) => (
+                    <h4><span>{key} :</span>{value}</h4>
+                ))}
             </div>
         </div>
     )
--- src/FetchData.js
+++ src/FetchData.js
@@ -1,31 +1,31 @@
-import {useEffect , useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card } from './Card';
-export const FetchData=()=>{
-    const [data, setData] = useState(null);
-    const [ loading , setLoading] = useState(true);
 
-    useEffect(()=>{
+export const FetchData = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
         axios.get("https://api.restful-api.dev/objects")
-        .then((response) => {
-            setData(response.data);
-            // console.log(response.data);
-            setLoading(false);
-        })
-    },[]);
-    if(loading){
+            .then((response) => {
+                setData(response.data);
+                setLoading(false);
+            })
+    }, []);
+    if (loading) {
         return <p>Loading..</p>;
     }
-    if(data == null){
+    if (data == null) {
         return <p>Error! </p>;
     }
-    return(
-        <>
+    return (
+
         <div className='CardList'>
-        {data.map((item) => {
-            return (<Card card={item}/>)
-        })}
+            {data.map((item) => {
+                return (<Card card={item} />)
+            })}
         </div>
-        </>
+
     );
 };
