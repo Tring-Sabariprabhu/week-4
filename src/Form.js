2b7a3929b94bc155e87f1d5c9f9e26acc07ca6c2
Tring-Sabariprabhu <sabariprabhu.a@tringapps.com>
2025-03-23 18:25:59 +0530

    edited css

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,10 @@
 
 import { useState } from "react";
-
 import './Form.css';
-
 
 function Form() {
     const [rows, setRows] = useState([
-        { name: "", location: "" }
+        { name: "", age: "", location: "" }
     ]);
 
     const handleChange = (index, event) => {
@@ -16,79 +14,74 @@ function Form() {
         setRows(newRow);
     };
 
-    // Function to add a new row
+
     const addRow = () => {
-        setRows([...rows, { name: "", location: "" }]);
+        setRows([...rows, { name: "", age: "", location: "" }]);
     };
 
-    // Function to remove a row
+
     const removeRow = (index) => {
         const newRows = [...rows];
         newRows.splice(index, 1);
         setRows(newRows);
     };
 
-    // Function to get all values
+
     const getValues = () => {
         console.log(rows);
     };
 
     return (
-        <>
-        
-        <table className="form">
-            <h1>Form with Multiple Inputs</h1>    
-            {rows.map((row, index) => (
-            
-                <div key={index} className="inputsRow">
-                    <tr>
-                    <td><input 
-                        className="item"
-                        type="text" 
-                        name="name" 
-                        placeholder="Name" 
-                        value={row.name} x
-                        pattern="/^[A-Za-z]+$/"
-                        onChange={(event) => handleChange(index, event)} 
-                    /></td>
-                    <td>
-                    <input 
-                      className="item"
-                        type="text"
-                        pattern="/^[0-9]+$/"
-                        name="number" 
-                        placeholder="Age" 
-                        value={row.age} 
-                        
-                        onChange={(event) => handleChange(index, event)} 
-                    /></td>
-                    <td>
-                    <input 
-                    className="item"
-                        type="text" 
-                        name="location" 
-                        placeholder="Location" 
-                        value={row.location} 
-                        pattern="/^[A-Za-z]+$/	"
-                        onChange={(event) => handleChange(index, event)} 
-                    /></td>
+        <form>
+            <h1>Form with Multiple Inputs</h1>
+            <table>
+                {rows.map((row, index) => (
+                    <div key={index} className="inputsRow">
+                        <tr>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Name"
+                                    value={row.name} 
+                                    onChange={(event) => handleChange(index, event)}/>
+                            </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="age"
+                                    placeholder="Age"
+                                    value={row.age}
+                                    onChange={(event) => handleChange(index, event)} />
+                            </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="location"
+                                    placeholder="Location"
+                                    value={row.location}
+                                    onChange={(event) => handleChange(index, event)} />
+                            </td>
 
-                    <td><button onClick={addRow} className="addButton item">+</button>
-                    {
-                        rows.length > 1 && <button type="button" onClick={()=>removeRow(index)}  className="removeButton item">-</button>
-                    }</td>
-                    </tr>
-                    
-                </div>
-            ))}
-            <tr><td>
-            <button onClick={getValues} className="submitButton">Submit</button>
-            </td>
-            </tr>
-        </table>
-        </>
+                            <td>
+                                <button type="button" onClick={addRow} className="addButton item">+</button>
+                                {
+                                    rows.length > 1 && <button type="button" onClick={() => removeRow(index)} className="removeButton item">-</button>
+                                    }
+                            </td>
+                        </tr>
+                    </div>
+                ))}
+                <tr>
+                    <td>
+                        <button type="submit" onClick={getValues} className="submitButton">Submit</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
     );
 }
 
-  
-  export default Form;  
+
+export default Form;  
